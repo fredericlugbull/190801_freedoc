6c5fddbe5f03a3b0914da3d3f9a627574ee2b8c7
Frédéric <f.lugbull@gmail.com>
2019-08-01 19:04:26 +0200

    yo

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,15 @@
 
 d= Doctor.create(last_name: "Dr Strange")
 p= Patient.create(first_name: "Le malade imaginaire")
-puts "j'ai créé mon doctor, #{d.last_name}, ainsi que son patient, #{p.last_name}"
+puts "j'ai créé mon doctor, #{d.last_name}, ainsi que son patient, #{p.first_name}"
 
+a= Appointment.create(doctor: d, patient: p)
+
+a.doctor
+
+a.patient
+puts "J'ai ensuite pris un rdv qui a permis de savoir à vie que ce rdv a eu pour médecin le #{d.last_name}, et que le patient lors de ce rdv était #{p.first_name}"
+
+d.patients
+
+p.doctors
